f36a1cceffec32359aa39e057bfb0f20a1019e11
Rían Errity <rian@paradaux.io>
2021-12-26 21:32:18 +0000

    Minecraft Development Boilerplate

---
+++ src/main/java/io/paradaux/dragonrace/DragonRace.java
@@ -0,0 +1,17 @@
+package io.paradaux.dragonrace;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class DragonRace extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
